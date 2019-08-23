30297db3962810b314323dfda7e61956fe9956c6
Marc Enschede <marc@marcenschede.nl>
2019-08-23 15:13:19 +0200

    Initial commit

---
+++ src/main/java/nl/marcenschede/tests/Gitflow/GitflowApplication.java
@@ -0,0 +1,13 @@
+package nl.marcenschede.tests.Gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/marcenschede/tests/Gitflow/GitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.marcenschede.tests.Gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
